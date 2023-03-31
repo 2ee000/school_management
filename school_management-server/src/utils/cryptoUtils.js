a0de05e199f216637cb58ae15867b3109bca6674
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-03-31 16:15:03 +0900

    create salt & hashedpassword use crpytoModules

---
+++ school_management-server/src/utils/cryptoUtils.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+const util = require('util');
+
+const pdkdf2Promise = util.promisify(crypto.pbkdf2);
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+
+module.exports = {
+
+    createSalt: async () => {
+        const new_salt = randomBytesPromise(64);
+        return new_salt.toString("base64");
+    },
+
+    createHashedPassword: async (password, currnet_salt) => {
+        const salt = currnet_salt;
+        const key = await pdkdf2Promise(password, salt, Number(process.env.KEY_STRETCHING), 64, "sha512");
+        const hashedPassword = key.toString("base64");
+        return { hashedPassword, salt };
+    }
+}
