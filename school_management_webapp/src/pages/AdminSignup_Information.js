98d1c0e6c159a25cdf077fef60c1c6b0cf7fcc1f
주현 <96381728+2ee000@users.noreply.github.com>
2023-04-01 18:56:00 +0900

    add signup function

--- school_management_webapp/src/pages/AdminSignup_Information.js
+++ school_management_webapp/src/pages/AdminSignup_Information.js
@@ -1,26 +1,83 @@
 import React from 'react';
+//import axios from 'axios';
 import '../styles/signup.css';
 import '../styles/signup_information.css';
 
 
 class AdminSignup_Information extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      adminName: '',
+      schoolName: ''
+    };
+    /*this.signupAxios = this.signupAxios.bind(this);*/
+    this.signupCheck = this.signupCheck.bind(this);
+    this.signupError = this.signupError.bind(this);
+    this.nextButton = this.nextButton.bind(this);
+  }
+
+  /*async signupAxios() {
+    await axios.post('서버',{
+      서버에서 admin name: this.state.adminName,
+      서버에서 school name: this.state.schoolName,
+    })
+    .then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }*/
+
+  async signupCheck(event) {
+    const name = event.target.name;
+    await this.setState({
+      [name]: event.target.value,
+    });
+    console.log(this.state[name]);
+  }
+
+  signupError() {
+    if(this.state.adminName === '') {
+      window.alert('Please enter the admin name!');
+      return;
+    } else if(this.state.schoolName === '') {
+      window.alert('Please select a school name!');
+      return;
+    }/*else {
+      this.signupAxios();
+    }*/
+  }
+
+  nextButton() {
+    console.log(this.state);
+    this.signupError();
+  }
+
   render() {
     return (
       <div className='admin-signup__app'>
         <p className='admin-signup__title'>Welcome, Create your school account</p>
         <div className='admin-signup__wrapper information__wrapper'>
-          <p information__wrapper--text>It is our great pleasure to have you on board!</p>
-          <input className='admin-signup__input' type='text' placeholder='Enter the name of admin'/>
-          <select>
+          <p className='information__wrapper--text'>It is our great pleasure to have you on board!</p>
+          <input className='admin-signup__input'
+          type='text' placeholder='Enter the name of admin'
+          name='adminName'
+          value={this.state.adminName}
+          onChange={this.signupCheck}/>
+          <select name='schoolName'
+          value={this.state.schoolName}
+          onChange={this.signupCheck}>
             <option>Select the name of school</option>
             <option>옵션1</option>
             <option>옵션2</option>
             <option>옵션3</option>
           </select>
-          <button className='admin-signup__button'>Next</button>
+          <button className='admin-signup__button'
+          onClick={this.nextButton}>Next</button>
           <div className='admin-signup__go-login'>
             <p>Already have an account?&nbsp;</p>
-            <a href='#'>Login</a>
+            <a href='/'>Login</a>
           </div>
         </div>
         <div className='admin-signup__step'>
--- school_management_webapp/src/pages/AdminSignup_Password.js
+++ school_management_webapp/src/pages/AdminSignup_Password.js
@@ -1,9 +1,65 @@
 import React from 'react';
+// import axios from 'axios';
 import '../styles/signup.css';
 import '../styles/signup_password.css';
 
 
 class AdminSignup_Password extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      adminPassword: '',
+      checkPassword: ''
+    };
+    /*this.passwordAxios = this.passwordAxios.bind(this);*/
+    this.passwordCheck = this.passwordCheck.bind(this);
+    this.passwordError = this.passwordError.bind(this);
+    this.nextButton = this.nextButton.bind(this);
+
+  }
+
+  /*async passwordAxios() {
+    await axios.post('서버',{
+      서버에서 admin password: this.state.adminPassword,
+    })
+    .then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }*/
+
+  async passwordCheck(event) {
+    const name = event.target.name;
+    await this.setState({
+      [name]: event.target.value,
+    });
+    console.log(this.state[name]);
+  }
+
+  passwordError() {
+    if(this.state.adminPassword === '') {
+      window.alert('Please enter a password!');
+      return;
+    } else if(this.state.checkPassword === '') {
+      window.alert('Please enter password confirmation!');
+      return;
+    } else if(this.state.adminPassword.length < 8 || this.state.checkPassword < 8) {
+      window.alert('Please enter a password of at least 8 characters!');
+      return;
+    } else if(this.state.adminPassword != this.state.checkPassword) {
+      window.alert('Please check your password!');
+      return;
+    } /*else {
+      this.passwordAxios();
+    }*/
+  }
+
+  nextButton() {
+    console.log(this.state);
+    this.passwordError();
+  }
+
   render() {
     return (
       <div className='admin-signup__app'>
@@ -11,14 +67,23 @@ class AdminSignup_Password extends React.Component {
         <div className='admin-signup__wrapper password__wrapper'>
           <div className='admin-signup__box'>
             <p>Choose a password</p>
-            <input className='admin-signup__input' type='password' placeholder='Enter the password'/>
+            <input className='admin-signup__input'
+            type='password' placeholder='Enter the password'
+            name='adminPassword'
+            value={this.state.adminPassword}
+            onChange={this.passwordCheck}/>
           </div>
           <div className='admin-signup__box'>
             <p>Confirm passwrod</p>
-            <input className='admin-signup__input' type='password' placeholder='Enter the password'/>
+            <input className='admin-signup__input'
+            type='password' placeholder='Enter the password'
+            name='checkPassword'
+            value={this.state.checkPassword}
+            onChange={this.passwordCheck}/>
             <p className='least-password'>Must be at least 8 characters.</p>
             </div>
-          <button className='admin-signup__button' type='submit'>Next</button>
+          <button className='admin-signup__button'
+          onClick={this.nextButton}>Next</button>
         </div>
         <div className='admin-signup__step'>
           <div className='step__icon'>
