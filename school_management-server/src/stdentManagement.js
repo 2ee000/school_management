d515bab3e6ec5a89fe6b5322bee60e890b2e2f33
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-04-04 16:00:27 +0900

    Update apidoc

--- school_management-server/src/stdentManagement.js
+++ school_management-server/src/stdentManagement.js
@@ -34,13 +34,42 @@
 
 
 /**
- * @api {post} /admin/ [POST] 관리자 등록하기
+ * @api {post} /admin/check-id [POST] 관리자 ID 체크
+ * @apiName 관리자 ID 체크
+ * @apiGroup Admin
+ * @apiVersion 1.0.0
+ * 
+ * @apiParam (Body) {String} admin_name Admin name
+ * @apiParam (Body) {Int} school_code School Code
+ * 
+ * @apiSuccess (Success 200) {Object} response Response of Success
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "statusCode": 200,
+ *       "message": "This ID is available"
+ *     }
+ *
+ * @apiError (Error 400) BadRequest AdminName is duplicate
+ * 
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *      "statusCode": 400,
+ *      "message": "Duplicate ID"
+ *     }
+ */
+
+
+/**
+ * @api {post} /admin/sign-up [POST] 관리자 등록하기
  * @apiName 관리자 등록하기
  * @apiGroup Admin
  * @apiVersion 1.0.0
  * 
  * @apiParam (Body) {String} admin_name Admin name
- * @apiParam (Body) {String} [password] this is saved from sha512 
+ * @apiParam (Body) {String} [admin_pwd] this is saved from sha512 
  * @apiParam (Body) {Int} school_code School Code
  * 
  * @apiSuccess (Success 201) {Object} response Response of Success
@@ -53,6 +82,55 @@
  *     }
  *
  * @apiError (Error 400) BadRequest Client's request was not correct
+ * 
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *      "statusCode": 400,
+ *      "message": "Bad Request"
+ *     }
+ */
+
+
+/**
+ * @api {post} /admin/login [POST] 관리자 로그인
+ * @apiName 관리자 로그인
+ * @apiGroup Admin
+ * @apiVersion 1.0.0
+ * 
+ * 
+ * @apiParam (Body) {String} admin_name Admin name
+ * @apiParam (Body) {String} [admin_pwd] this is saved from sha512 
+ * 
+ * @apiSuccess (Success 200) {String} Token AccessToken 
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZDgwYzE5Yzk3ZTcwNGRlMjhmNDEiLCJpYXQiOjE2ODA1MDA0NzcsImV4cCI6MTY4MDUwMDUzN30.uCp80fhtVliTVVpKU7CKmK-KHhwtvSYTDvQJs2Piev0"
+ *       "statusCode": 200,
+ *       "message": "Success login"
+ *     }
+ * 
+ * @apiError (Error 403) Forbidden accessToken was invalid.
+ * 
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 403 Forbidden
+ *     {
+ *      "statusCode": 403,
+ *      "message": "Forbidden"
+ *     }
+ * 
+ * @apiError (Error 401) Unauthorized refreshToken is invalid
+ * 
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Unauthorized
+ *     {
+ *      "statusCode": 401,
+ *      "message": "Unauthorized"
+ *     }
+ * 
+ * @apiError (Error 400) BadRequest admin_name or amdin_pwd is not correct.
  * 
  * @apiErrorExample Error-Response:
  *     HTTP/1.1 400 Bad Request
@@ -202,3 +280,51 @@
  */
 
 
+/**
+* @api {get} /school/all [GET] 모든 학교 찾기
+* @apiName 모든 학교 찾기
+* @apiGroup School
+* @apiVersion 1.0.0
+*
+* @apiSuccess (Success 200) {String} school_code School code
+* @apiSuccess (Success 200) {String} school_name School name
+* @apiSuccess (Success 200) {String} school_email School email
+* @apiSuccess (Success 200) {Object} response Response of Success
+* 
+* @apiSuccessExample Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*       "data": [
+        {
+            "school_code": 1,
+            "school_name": "가나고등학교",
+            "school_email": "gana.ac.kr"
+        },
+        {
+            "school_code": 2,
+            "school_name": "가천고등학교",
+            "school_email": "gachen.ac.kr"
+        }
+        ],
+        "statusCode": 200,
+        "message": "Success to find all schools"
+*     }
+*
+* @apiError (Error 404) SchoolNotFound The admins were not found.
+* 
+* @apiErrorExample Error-Response:
+*     HTTP/1.1 404 Not Found
+*     {
+*      "statusCode": 404,
+*      "message": "Schools were not found"
+*     }
+*
+* @apiError (Error 400) BadRequest Client's request was not correct
+* 
+* @apiErrorExample Error-Response:
+*     HTTP/1.1 400 Bad Request
+*     {
+*      "statusCode": 400,
+*      "message": "Bad Request"
+*     }
+*/
